7d80d5b3a349dd8e15abc4bbcd63ad10b85976b7
David Axelrod <daxaxelrod@gmail.com>
2017-01-23 18:43:15 -0500

    end of day 1-23-17

---
+++ Fin_320/1-23-17.py
@@ -0,0 +1,74 @@
+Financial markets and institutions
+1-23-17
+
+3 Elements to the Financial system
+    1 Financial instruments --> what # bonds stocks and assets financial claim
+    2 Financial markets --> where # nyse and nasdaq
+    3 Financial institutions --> who # banks and funds
+
+There are several different classifications for markets
+    1. Primary markets
+        issuesrs
+        typically involve investment bank who underwrites teh offering
+    2. Seconday markets
+        investors
+        #think after ipo, NYSE and nasdaq
+        involves brokers and dealers
+            dealer owns supply and maintain assets
+            broker does not own the assets
+        Secondary markets provide liquidiity and more information about the company
+            liquidiity == selling quickly at the market price
+
+    Money markets -- short term
+        less than a year
+    capital markets -- long-term
+        > 1 year
+
+    debt markets
+        all maturity levels
+    equity markets
+        pay dividends, in theory forever
+        represents an ownership in the firm
+
+Asset Classes
+    1 Stocks: Equity
+        return is a dividend
+    2 Bonds: Debt
+        return is a coupon
+    3 Currency: Money
+        retirn is interest
+    4 real estate
+        rent
+    5 assets
+        lease payments
+
+Financial Intermediaries: Indirect finance
+    Transaction costs
+        1 Financial intermediaries make profits by reducing transaction costs
+        2 reduce transaction costs by developing expertise
+
+    They also help reduce the impact of asymmetric information
+    if one party lacks crucial information about another party
+        this impacts decision making
+    this issue is also known by 2 names
+        adverse selection and moral hazard
+
+    Moral hazard is after the transaction occurs
+        hazard that borrower has incentives to engage in undesirable (immoral) activities
+        another example is insurance
+            people are more likely to take on more risk after being insured
+
+Types of financial institutions
+    Banks
+        commercial
+        savings and loan associations
+        mutual savings Banks
+        credit unions
+    Contractual savings institutions
+        insurance
+            life
+            fire and casualty
+        pension funds
+    Investment intermediaries
+        Finance companies
+        mutual fundsmoneu market and mutual funds
