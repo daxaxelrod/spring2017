49a8edfd1546ebf0d69235107ab64237add3f5df
David Axelrod <daxaxelrod@gmail.com>
2017-01-23 16:45:04 -0500

    added new model field for if the trainer logged in that day and route to handle a reset at the end of the day. useful for the plant reward system

--- Acct 212/1-23-17.py
+++ Acct 212/1-23-17.py
@@ -2,7 +2,27 @@ Accounting 212
 1/23/17
 
 Do whatever you can do textbook wise, just do it
+Saying of the day: Cash is king
 
 Adgenda
 Role of CFO
 Break into small groups
+
+Managerial Acct focuses on the future
+
+Role of a good CFO
+    Planning
+    controlling
+    Decsion making
+
+Organizations have 4 moving pieces
+    Accounting and Finance
+    Marketing and Advertising
+    Operations and Managment
+    Human Resources and Organizational Behavoir
+
+
+
+Notes
+Some kid in the back cuts hair
+        Candidate for elc
