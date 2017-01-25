4c9e2e1754a11eb4103340520b009a4dd1867ecb
David Axelrod <daxaxelrod@gmail.com>
2017-01-25 14:01:14 -0500

    notes through midday wednesday 1-25-17

---
+++ Acct_212/1-25-17.py
@@ -0,0 +1,40 @@
+Acct 212
+1-25-17
+
+Problems for friday
+    p4, 13, 9
+    problems 1-21, 1-23 1-25
+
+Page 11 From the prolouge
+
+IMA (institue of managerial accounting)
+    Has some sort of ethics code
+    Without a code of ethics, you won't s'urvive
+    compete on a fair and open playing field
+
+You have to be competant at what you do. you must maintain a high level of
+expertise and skill
+
+CPE - continueing professional education
+Confidentiality
+    Dont share information
+    duh
+
+If you have an ethical conflict, go to your supervisor
+If therer is a clear violaion of the law, just go to the feds
+
+
+#########
+Chapter 1 - Cost flows
+#########
+
+Product costs and period costs
+Inventory does not appear on the income statement until its sold
+We capture out product costs in inventory
+
+if you spend 1 million to make something and dont sell it, you have no expenses
+Only when you sell something do the expenses count
+That 1 million live in inventory
+
+SGA is always taken out in the income statement
+    not attached to the product
--- Hist104/1-25-17.py
+++ Hist104/1-25-17.py
@@ -6,3 +6,82 @@ Potential Ids
         Railroad car
     Vertical and horizontal integrations
     old and new immigrants
+
+Industrialization && immigration
+    Chicago was raised for a sewer system anecdote
+
+    Us becomes the worlds largest industrial power next to only great britain
+    us gnp goes from 9 billion to 27 in 30 years
+
+    immigration yeilds 25 million + population
+    25% -33% of people in american cities are foreign born
+
+New techs
+    Telegraph
+        Comms, brah
+    Sewing Machines
+        Stop pricking yourself people
+    Light bulb
+        Let there be light
+    Steam ships
+        4 times faster than traditional boat
+    Type writter
+        Leads to an increase in typing speed .... I guess - Professor
+    New petroleum refinement
+        John D rockefeller is a large player and owns 90%+ of the market
+    Bessemer Process
+        Turns pig iron into steel
+        Removes iron impurities from the iron, allows manufacture of mass steel
+    Railroad
+        Improves transcontinental travel speed
+        They requested a standardization of time (split us into the 4 tzs)
+
+        refridgerated car
+            Exactly what it sounds like
+    Taylorism (factory managemet)
+        Emphisiszes numbers and efficiancy
+
+Gustavus Swift (Swift and co)
+    Creates meatpacking plants and a lot of the infrastructure
+    Put distribution centers in different cities
+    Aggresively advertises
+    No waste from production, makes glue from discarded items
+
+Vertical integration
+    think spacex
+    one corp owns all facets of production
+horizontal integration
+    Control a specific step in the manufacturing process
+
+Federal Governments role
+    Gives land, subsidies and protective tariffs and provides patents
+        Want the nation to suceed
+    Doesnt want monopolies however
+        The laws they pass dont do much
+        Interstate commerce act
+            Establishes commision to oversee interstate railroads
+            supreme court cuts it down
+        Sherman anti trust act
+            Outlaws trusts and monopolies
+            also is against fixed prices
+            Supreme court defended business interests
+        After the sherman act is passed, Us sees record M & A activity
+        ^^^LOLOLOLOLOLOLOLOLOLOLOLOLOLOLOLOLOLOLOLOLOLOLOLOLOLOLOLO^^^
+
+Immigration
+    Most immigrants end up in cities
+    throught the world, 60 million people move from farms to towns/cities
+    People leave their home country for economic and persecutory reasons
+
+    Chain migrations
+        People of the same ethnic background invite their old neighbors
+            and work in similar industries
+        Some sent money back home
+    by 1920, half of people live in cities
+    there is also the rise of suburbs for richer individuals
+
+    they live in dumbell tenements
+        People threw their trash in middle courtyard
+    Huge amounts of disease
+    They also have a lot of sex -> children
+    Large risk of fire
