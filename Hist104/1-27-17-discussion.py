af0418d4a04e5513adf7a1156f0895ab6812d923
David Axelrod <daxaxelrod@gmail.com>
2017-01-30 12:41:10 -0500

    latest as of 1-30-17

--- Hist104/1-27-17-discussion.py
+++ Hist104/1-27-17-discussion.py
@@ -2,3 +2,4 @@
 Hist 104 discussion
 
 Kinda useless to take notes
+Gets through the class by demonizing owners of companies
---
+++ Hist104/1-30-17.py
@@ -0,0 +1,85 @@
+Hist 104
+1-30-17
+
+Id's'
+    Exodusters
+        name given to African Americans who migrated from states
+        along the Mississippi River to Kansas in the late nineteenth
+        century, as part of the Exoduster Movement or Exodus of 1879.
+         It was the first general migration of blacks following the Civil War.
+    Homestead act 1862
+    The frontier
+    the carlisle school
+    chinese exclusion act 1882
+
+Maps in the late 19th centrury shows the west as empty and fit for conquest
+
+post civil war, west seems like an area of opportunity
+    #just like space is going to be after wwIII
+    Maniest Destiny == God says fucking go for it
+
+Reality of the west
+    Was not as empty as people thought it was
+    more urban than east because of central infrastructure (people lived near trains)
+    depended on federal government
+        especially true for railroads
+        gave railroads 175 million acres
+
+Why people move west
+    Farming
+    mining
+    ranching
+
+Homestead act
+    1 out of 10 Homesteads were actually used for farming
+        the rest were speculators
+    you can get 160 acres for $10
+        225.72 in todays money
+            shit thats cheap
+    400k people take the gov up on this offer
+    Other useful acts
+        Timber Culture act
+            Plant trees for free land
+        Desert Land act
+        Timber and stone act
+            sell your land as a mine or timber site if there is no farming value
+
+mining
+    Cali gold rush was early (1849)
+    Massive wealth for capitalists who financed successful mines
+        Some also lost everything but history doesnt remember losers now does it
+    Majority men
+        Prostitutes where around
+
+Ranching
+    They slaughered bison
+    Cattle was raised here to be sent to slaugher houses like chicago
+
+Black Exodusters
+    In response to failed reconstruction, freedmen and women leave to go westward
+    They wouldnt recieve as many benifits as whites (less preference for the homestead act)
+
+Chinese Immigration
+    330k+ moves to the west between 49 and 82
+    mostly single men
+    did a lot of "women work"
+    financial panic in 1873 turns many people against the Chinese
+    Chinese Exclusion act 1882
+        First immigration restriction in the us
+        repealed in the 1940's'
+
+Mexicans and Mexicans-Americans
+    Treaty of Guadalupe Hidalgo
+        Ends mexican american war 1846-48
+        Allows mexicans to retain citizenship
+    They often had their land taken away
+    Victims of lynchings
+        597 to be exact
+
+Native Americans
+    Concentration moves Na's' onto reservations
+    Oklahoma was just for NA's' when it was established
+    Congress forms no more treaties with NA post 1871
+    Tons of Disease on these reservations
+    Starvation caused by lack of bison
+        Thanks American assholes
