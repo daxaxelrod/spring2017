630f3c8737415fad5d27670facfc27f83c542aa3
David Axelrod <daxaxelrod@gmail.com>
2017-01-31 17:35:15 -0500

    last of finance notes

---
+++ Fin324/1-31-17.py
@@ -0,0 +1,25 @@
+Fin 324
+1-30-17
+
+Starts out with current events
+Discusses how a 20% mexican good tarriff
+    Effects cogs
+    could effect rev
+
+Replacement -- replacing an old machine with a new one
+    Rembeber we only focus on incremental cash flows
+
+Capital structure
+    Debt and equity a firm uses
+    leverage = adding debt to the capital struct
+
+Capital Restructuring
+    Changes the mix of debt and equity
+    keeps the net value of the company static
+
+The effect of leverage
+    In a good year, profits are amplified
+        Interest payments are fixed so any benifit from the leverage goes further down the income statement
+    In a bad year you screwed the pooch
+        Interest payments wiegh on profits
+    
