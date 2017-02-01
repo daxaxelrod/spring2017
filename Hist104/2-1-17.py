68ffe72a1b7105b5febd6b577feb67c0d8cfa266
David Axelrod <daxaxelrod@gmail.com>
2017-02-01 13:07:59 -0500

    notes from history for the first of feb

---
+++ Hist104/2-1-17.py
@@ -0,0 +1,92 @@
+History 104
+2-1-2017
+
+Populism
+    One of the most important politica
+
+IDs
+    Political machines
+    Farmers alliances
+    Free silver
+    Election of 1896
+
+Agrarian discontent
+    People felt that just a few people were getting wealthy
+    Farmers felt like they were unduely effected by the events at the time
+
+Gilded age of america
+    the thought that america had a gold face but a dirty interior
+
+Republicans at the time were pro-business
+    Tarriff is desireable
+Democrats are pro immigration restrictions
+    No Tarriff
+
+Caused a period of stalemate
+
+Political machines
+    Mostly urban phenomenon
+    target immigrants off the boat
+    Pros
+        Great voter turnout
+        empowered the poor and immigrants
+            Helps to provide food and health insurance
+    Cons
+        Massive corruption and voter fruad
+
+Populism
+    several emerge
+    2 movements have similar purposes
+    The grange
+        in ten years since start, grew to 1.5 million members
+        targets railroads
+        Hope to circumnavigate bankers brokers and merchants
+    Farmers alliance
+        split into 3 minor groups
+            Southern Northern and colored
+        Emphasized self help, cooperatives and mutual aid societies
+        Politically active, elect a ton of reps to the local and state level
+
+    Their platform
+        A tarriff reduction
+        cheaper grain storage and rail road prices
+        Graduated income tax
+        unlimited coinage of silver
+        direct election of senators
+        woman right to vote
+        public owner of railroads
+        8 hour work day
+
+Women would also be involved in the movements
+    mary e lease was a major figure
+    was trying to fight poverty as a whole
+
+They get together in 1892 to actually form a party
+    The platform is similar to the farmers alliance platform
+    their presidential candidate wins 1+ million votes (8.5%)
+    10 congressment, 5 senators and 3 governers
+
+Panic of 1893
+    prices plummet for farmers
+    20-25%  industrial unemployment
+    Gold reserve depleted
+    Too much rail road speculation
+    mass run on banks
+
+Free silver
+    Debate begins with the aforementioned panic
+    Deflation was cause by a growing number of people using the currency pitted against a constant monetary pool
+    Bring silver into the picture
+
+The next election lead to a political realignment that lasts until the 1930s
+    Money and currency was the main issue
+
+William jennings bryan
+    Ardent free silverist
+    "the great commoner"
+    Gives a famous cross of gold speech
+    Repubs fight against him intensly
+        See him as hardful to the nations new preosperity
+    Gave 36 speeches in one day. What a boss
+    Fails to win presidential election
+        Doesnt appeal to industrial class which is where a huge voter block
