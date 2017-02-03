67c5a8f22151ed2e81a943679268079730c61f0d
David Axelrod <daxaxelrod@gmail.com>
2017-02-02 20:53:21 -0500

    notes through 2-2-17

---
+++ Fin324/2-2-17.py
@@ -0,0 +1,15 @@
+Fin 324
+2-2-17
+
+In the last class
+    we started with a company that doesnt have any debt in is capital structure
+    With debt would give higher eps in future years
+
+From the 1958 Theory
+Main takeaway is that capital structure really isnt that relavant
+    The operating cfs are not effected
+The cost of equity increases linearly with the debt to equity ratio
+
+
+So the only way to increase the value of the firm is to increase the value of the cashflows
+or minimize the wacc
---
+++ Fin_320/2-1-17.py
@@ -0,0 +1,26 @@
+Fin 320
+2-1-17
+
+Interest rates carry two types of risk
+    Macro or market risk
+        Interest-rate risk
+        Reinvestment risk
+    Bond itself
+        Default risk
+        prepayment risk
+
+Rate of return formula
+    Coupon + Capital Gain
+           /
+         Price
+
+Fixed payment has higher Reinvestment risk
+zero coupon bonds have higher interest rate risk
+
+Real interest rate
+    Expected inflation rate is pi to the e
+
+Going over a few true false questions
+
+basic idea is in this question
+Increasing duration implies that interest-rate risk has increase
