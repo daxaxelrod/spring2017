8f5301a517fde319afacb43836ea20c680d5581a
David Axelrod <daxaxelrod@gmail.com>
2017-02-06 18:49:47 -0500

    fin 320 notes from 2-2-17. Ran out of battery on my tablet but there wasnt much more to write down

---
+++ Fin_320/2-6-17.py
@@ -0,0 +1,54 @@
+Fin 320
+2-6-2017
+
+Chapter 4
+
+Why do intereest rates change
+
+Determinants of asset demand
+    Wealth
+    Expected return
+    Risk
+        Assume risk averse
+    liquidity
+
+Supply and demand in the bond market
+    Example
+        1 Year discount bond at a face value of $1,000
+        return = (face value - price) / PRICE
+        price = $950
+        return = 5.3%
+        Assume demand is 1000 billion
+        She then chart the point of 950 on yaxis and 1000
+            on the xaxis
+        Now assume that the price of the bond decreases to $900
+            and the demand for the bond increases to 3000
+        She does a similar thing with the price at 850
+
+    Same thing happens with supply
+        if you have a decrease in return (increase in price)
+            The supply of bonds on the market will increases
+
+What causes a shift in the demand curve
+    Wealth increases
+        Right shift
+    Expected return increases
+        Shift to the left
+    Inflation increases
+        Left
+    riskyness increases
+        Demand shifts left
+    liquidity increases
+        You can sell it easier
+        Demand shifts to the right
+
+Supply curve shifts
+    profitability increases
+        Supply shifts left b/c people are more willing to borrow
+    Expected inflation increases
+        supply increases
+    government deficit
+        if the government is spending more than its bringin in, it will sell more bonds
+        which drives up supply shifting it to the right
+
+She then runs through several exmples of supply and demand curves being affected
