b49d498d2a314e259bb917b5cf4dc759d7e9dcef
David Axelrod <daxaxelrod@gmail.com>
2017-02-06 13:00:34 -0500

    hist104 on 2-2-17

---
+++ Acct_212/2-3-17.py
@@ -0,0 +1,12 @@
+Acct 212
+2-3-17
+
+Chapter 2 and a little bit of 3
+
+Gross margin is just the difference between sales and cogs
+    Needs to cover s g&a
+
+2 Types of formats
+    Standard income statement
+    Contribution format
+        Shows behavoir of costs, not the funciton of costs
---
+++ Hist104/2-6-17.py
@@ -0,0 +1,130 @@
+Hist 104
+2-6-17
+
+ID's'
+    Unskilled vs skilled labor
+    Knights of Labor
+    AFL -- American Federation of Labor
+    NWTUL -- National Women’s Trade Union League
+    The great upheaval
+    Shirtwaste factory strike
+
+State troop break strikes over 500 times
+    Laborors are challenging industrial capitalism
+
+How did labor start to change
+    Goes from 885k to 3.2 million
+    rise in large scale production
+    Rise of wage labor
+        Makes you dependant on the employer
+
+Skilled vs Unskilled
+    Piecemeal repetitive work that anyone can learn
+    you can pay them less
+    new hieracrchy of jobs based on age, gender, race and ethnicity emerges
+    White men weree often overseers and generally higher in the food chain
+
+1 out of 6 people in the railroads often ended their career with an injury
+
+Women and children enter the factories
+    Percentage of women in the workforce triples
+    Most likely to work in the textile or garment industries
+    Large pay disparitites -- paid less than male counterparts
+    by 1890 1.5 million children employed
+
+Workers organize in response to poor conditions
+    Ethnic and racial divides are apperant
+    Unions often differentiate between skilled and Unskilled
+    Lack political voice
+    Federal government sides with business interests
+        Who pays taxes yall?
+    Most workers dont have the savings to go on strike
+    Social darwinism -- you won because you're' better than other individuals
+
+8 hour workday
+    federal employees had an 8 hour day
+    No national legislation so labor fights were segmented by industry
+
+Knights of labor
+    Included skilled and unskilled
+    Founded in 1869 in phili
+        Terrence V. Powderly was in charge
+    Wanted
+        8 Hour workday
+        End to child and convict labor
+        graduated income taxes
+        Coop business model
+        700000 in 1886
+        Brought down by Haymarket Bombing
+            Kills 6 Policemen
+            Assumed radicals
+
+American Federation of Labor (AFL)
+    Founded in 1886 (same year knights of labor peaked)
+    1.6 million members by 1904
+    Federation of individual trade Unions
+    Excluded non-white men
+    Wanted
+        Higher Wages
+        less hours
+        better working conditions
+    They were less interested in political power
+    More conservative labor org
+
+Industrial Workers of the world
+    Started in 1905
+    Most active in the west
+    Seen as the next step from the Knights of Labor
+        More radical however
+    Preached class warfare, Marxist bent against the employer class
+    Lots of strikes
+
+National Womens trade Union League
+    Kenny o-sullivan
+    United wealthy reformist and working class women
+    Supported living wage for women
+    Helped provide relief during strikes
+
+Socialist party of america -- not a union
+    150k members
+    323 socialist newspapers
+    they elect more than 1000 people by 1920
+    less radical than the Industrial Workers of the world (IWW)
+    wanted reform, not revolution
+
+between 1880 and 1901 there were 37k strikes
+
+The great upheaval (1877)
+    Great railroad strike
+    Begins in West virginia
+    100k rail road workers go on strike
+        Nation wide
+        2/3s of railroads are lying idle
+    Last 45 days broken up by president Hayes with federal troops
+    Stokes fears of working class revolution
+
+Thibodaux Massacre
+    sponsored by knights of labor
+    black sugar cane harvestors
+        10k walk off the job
+    Turns into a race riot
+        Really violent 30 killed, 200 injured
+
+Pullman strike (1894)
+    pullman made sleeping train cars
+    protesting 3 wage cuts in 1 year
+    700 cars burned, 13 dead 53 wounded
+
+Shirtwaste strike
+    Supported by womens trade union League
+    Mostly women working in Shirtwaste factories
+    First successful female labor uprising in america
+        They get better wages shorter hours and equal treatment
+
+Ludlow Massacre (1914)
+    Organized under the united mine workers of america
+    Ludlow, Colorado
+    Miners wanted better wages, 8hr day and right to unionize
+    national gaurd hits tents with machine gun fire
+    kills 14 women and children, 6 miners
+        People were pissed
