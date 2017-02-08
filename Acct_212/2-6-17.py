201d9b0ef6e8a434b9bf0fe81cb31375c283cc53
David Axelrod <daxaxelrod@gmail.com>
2017-02-08 12:59:03 -0500

    history notes from 2-8-17

---
+++ Acct_212/2-6-17.py
@@ -0,0 +1,29 @@
+Acct 212
+2-6-17
+
+Job order costing -- Chapter 2
+    Calculating unit product costs
+    how to measure and identify unit costs
+
+Companies that can use job-order costing
+    Boeing
+    Large scale construction
+    movie production
+
+POHR
+    Rate used to apply to overhead jobs is determined before the period begins
+    Estimated total manufacturing overhead cost for comming period
+        divided by
+    Estimated total units in the allocation base for the coming period
+
+The need for POHR
+    accurate accounting
+    can adjust the value later when real values come in
+    Calculated by y = a + xb
+    a = Estimated fixed
+    x = variable multiplier
+
+Choosing an allocation base
+    Very important
+
+Chapter 3
---
+++ Hist104/2-8-17.py
@@ -0,0 +1,93 @@
+Hist 104
+2-8-17
+
+Ids
+    Jim Crow
+    Plessy v Ferguson
+    New South
+    Atlanta Compromise
+    Ida B. Wells
+
+Major Qs
+    How did white southerners retain power in the south
+
+Plessy V Ferguson
+    Plessy was 1/8th black
+        Called an octaroon
+    Boarded a train
+    Goes onto the train hoping to be arrested
+        Wanted to challenge an act called the seperate car act (People of color gets different cars)
+    He told the conductor that he was 1/8th black and then refused to move black
+    Comes before the supreme court in 1896
+    Segregated cars called jim crow cars
+    8 out of 9 judges said that seperate but equal was constitutional
+
+South in the industrial era
+    Destruction was cray
+        1/3 of all buildings were destroyed
+        Lagged behind north
+    Enters era 4 decades behind north
+    South makes up only 10% of national manufacturing output by 1900
+    20% of southern taxes paid pensions for confederate war vets
+        Jackasses
+
+Attempts to industrialize
+    People who want to industrialize the south and create a new south
+        Henry Grady (Big fan of new south)
+    Others want to be stagnant and maintain agriculture as a main source of income
+
+    Attempts to convince northerners that the south is now civilized and is done with slavery and secesion
+        And thus less risk than initially perceived
+
+South tries
+    Uses prisons for free labor
+    free land for railroads
+    Birmingham alabama started making a crap ton of pig iron
+
+    Other major industries include tobacco and textiles
+        Wages were much lower than in the north
+        .11 an hour
+
+Jim crow
+    Not an actual guy
+    Was an offesnsive character and then became main stream
+
+    Political
+        Disenfranchising poor people, not just blacks
+        Poll taxes (You have to pay a fee to vote)
+        Grandfather clause (Your grandpa had to be able to vote for you to vote)
+        Literacy tests (Pretty obvious what this one is)
+        All white primaries (Only whites can run for office)
+        Williams v Mississippi upheld the restrictions outlined above
+
+        By 1910, only 3% of blacks were eligible to vote
+    Economic
+        Sharecropping
+        Crop lien system
+            Renting to own farmland. Kept people dependant
+        Contract Labor
+
+    Social Control
+        Blacks imprisoned often and then used for convict labor (think chain gangs)
+        Formal Segregation
+            Can be arrested for small infractions
+        Also were rarely cival servants
+            Small number of black cops and firefighters
+        Too many lynchings
+            Between 1885-1918, roughly 5000 blacks murdered because of violations of racial codes
+
+    Atlanta Compromise Speech
+        At exposition to show the new south to investors
+        800,000 people go over 3 month
+        Pleads african americans to wait for equality and to work their way up the social ladder
+        "Cast down your bucket where you are"
+            We are a great source of labor for your industrial dream o white southerners
+
+    Ida B Wells
+        A journalist who wants to shame people into changing the system
+        In Mississippi
+        Daughter of 2 slaves
+        Launches crusade against lynchings
+            Writes a huge document that kept a record of lynchings
+        Highly critical of white suppression of black talent
+        Goes all over the world to raise awareness
