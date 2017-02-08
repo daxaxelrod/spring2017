eb25a7e614ac629d249ea80775c2bde4e0746ba8
David Axelrod <daxaxelrod@gmail.com>
2017-02-08 14:02:58 -0500

    acct 212 for 2-8-17

---
+++ Acct_212/2-8-17.py
@@ -0,0 +1,25 @@
+Acct 212
+2-8-17
+
+Giving out the case friday
+
+Chapter 3
+    The split between wip and manufacturing overhead
+    manufacturing overhead includes
+        Direct materials
+        Indirect labor
+        Other overhead
+            Includes taxes and factory depreciation
+            Anything that belongs to the factory belongs in manufacturing overhead
+            Must be zeroed out at the end of any period
+
+    Depreciation Expense
+        Contra asset
+        Debit is different depending on what you're ' depreciating, but the credit is always the same
+
+    How to compute overhead costs
+        At the end of the period manufacturing overhead has to be zeroed
+        Disposed in one of two wayes
+        It can be closed to cost of goods sold
+        Or
+        It can be closed proportionally to work in progress, Finished goods and cost of goods sold
