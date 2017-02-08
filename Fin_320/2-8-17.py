917b2d48cdffd94174ab51b5b4c27303d49af3f6
David Axelrod <daxaxelrod@gmail.com>
2017-02-08 18:38:41 -0500

    through then end of the day on 2-8-17

---
+++ Fin_320/2-8-17.py
@@ -0,0 +1,8 @@
+Fin 320
+2-8-17
+
+Last class was all about supply and demand of the bond market
+
+She continues to go over effects on bond supply and demand for the whole class
+    There is a packet which she gives us that has a few sample problems
+    But lets face it. tibah is much more interesting
