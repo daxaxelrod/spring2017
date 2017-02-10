5c18ec031d97a1c98c6dfc3d7de2ad5a082c47b1
David Axelrod <daxaxelrod@gmail.com>
2017-02-10 13:15:54 -0500

    notes through 2-9-17

---
+++ Fin324/2-9-17.py
@@ -0,0 +1,35 @@
+Fin 324
+2-9-17
+
+Pecking order theory was discussed last class
+Absent last class as well
+Donaldson 1961
+    Managers perfer to finance with retained earnings
+        Then with debt followed by equity
+
+Recap of what we'v'e done so far
+    ***Capital Structure theory***
+        MM theory without taxes -- Miller and Modenomy <- butchered the second name
+        :firm value is independent of the capital structure
+        : Wacc is not dependent on capital structure
+            levered beta is a function of unlevered beta
+        : cost of equity can be written as risk free rate + Beta(1 + debt/ equity)(Market risk premium)
+
+    ***Trade off theory of capital structure***
+    value of levered firm = value unlevered + debt interest * tax rate - (Cost distress and bankruptcy)
+
+    ***Pecking order theory***
+        Considers frictions
+        -> Adverse selection
+            Firms try to rely on internal funds first
+            Debt next and equity as a last resort
+        -> transaction costs
+        Evidence for pecking order
+            Tax benifits from debt. $1 debt adds $.10 to value
+            Graham argues that a typical firm can double tax benefits by issuing more debt
+            Why its not .35 and is .10
+                There are costs with taking more debt
+                Bankruptcy costs and financial distress are important
+                Some financial people state that for a BBB firm, the NPV of ditress 4.5% of pre-distress value
+
+Value of unlevered firm = Ebit (1-T)/ Rate(u)
--- Fin_320/2-8-17.py
+++ Fin_320/2-8-17.py
@@ -6,3 +6,16 @@ Last class was all about supply and demand of the bond market
 She continues to go over effects on bond supply and demand for the whole class
     There is a packet which she gives us that has a few sample problems
     But lets face it. tibah is much more interesting
+
+There is a 99% chance there is going to be a duration and price % change problem on the test
+
+Chapter 5
+    Several reasons for bonds not all being priced the same
+    There is default risk in the if a debtor defaults on his or her debt
+        T bills have not risk premium because they have no default risk (They can just print more money)
+
+    Risk premium
+        Pays aditional interest so that you are willing to hold a riskier bond
+
+    Default Risk
+        
