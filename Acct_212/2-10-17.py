6d003b5b8ec23099a26a4108f3ecfc6b4e1283a0
David Axelrod <daxaxelrod@gmail.com>
2017-02-12 01:48:32 -0500

    homework at a hackathon. Almost done with it, just need to do the additional problems

---
+++ Acct_212/2-10-17.py
@@ -0,0 +1,11 @@
+Acct 212
+2-10-17
+
+Goes over the schedule for the case
+This is a 20 freakin minute long case
+    ugh
+
+Chapter 4
+    Again manufacturing overhead estimant
+    Spent the entire class going over one problem
+    The solution is on black board
