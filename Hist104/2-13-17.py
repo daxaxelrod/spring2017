53d24f67564e8c671c27ae48af4c366a69d4b9af
David Axelrod <daxaxelrod@gmail.com>
2017-02-13 12:58:51 -0500

    history notes from 2-13-17

---
+++ Hist104/2-13-17.py
@@ -0,0 +1,51 @@
+Hist 104
+2-13-17
+
+Id's'
+    Spanish American war
+    Rosevelt correlary
+    Crisis of Masculinity
+    Yellow Journalism
+    Anti-Imperialist League
+
+Us wanted to get in on the imperial game.
+    Europe had carved up africa since the meso american colonies rebeled
+    Some economst (william stewart) pushed for american exports
+    Racial hieracrchy also lead to the idea that imperialism helped the uncivilized
+    Social darwinism
+    USA Starts to build up a navy
+
+Crisis of Masculinity
+    Economic growth led to middle class
+        Felt people had gotten too soft (that they were being pussies)
+    Felt that a stronger more manful race could take america down
+
+Spanish American War
+    Cuba is a spanish colony
+    Only 2 months long
+    After the economic crash, revolutionist emerge. Spanish squash it
+    Us has ~ 50 mil invested in Cuba
+    displayed as humanitarian Crisis
+    Uss maine is sunk in the havana harbor
+
+def Yellow Journalism:
+    Sensationalist stories
+    Published in newspapers
+
+American Anti-Imperialist League
+    WEB Dubois, Canegie, jane adams and mark twain were memebers
+    25k members
+    against philppine war
+        1. War is immoral and would corrupt the US
+        2. US had way too many problems at home
+
+China
+    All european parties were vying for power in china
+    US advocates Open door policy
+        Ensure equal access to the chinese markets
+        US helps to put down the boxer rebellion
+
+Rosevelt Correlary
+    US would protect the sovernty of the latin america from colonial powers
+    Makes America the International Police men
+    Rise of dollar diplomacy
