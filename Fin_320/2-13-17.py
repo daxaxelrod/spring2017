0d0b983639aa8acab950894c4e4ea81de4e27ab4
David Axelrod <daxaxelrod@gmail.com>
2017-02-13 21:17:04 -0500

    notes though 2-13-17

---
+++ Fin_320/2-13-17.py
@@ -0,0 +1,25 @@
+Fin 320
+2-13-17
+
+No projector today
+
+Yield Curve
+    Two bonds with the same liquidiity,
+    risk and payment but with different maturities
+    Upward sloping log curve
+
+This class is on the 3 theories of yield curves
+    And how it gives us the current economic condition
+
+Market Segmentation theory
+    We cannot substitute one bond for another
+
+expectation theory
+    The short term bonds can perfectly substitute the long term bonds
+    rate_1 + rate_1_2^^x / 2
+
+
+Liquidiity Premium Theory
+    Connected market Segmentation and expectation theory
+    Long term interest rate should be average of short term interest rates
+        Plus a liquidiity premium
