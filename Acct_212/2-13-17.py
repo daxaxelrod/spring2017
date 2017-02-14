9c99d2199771ad461cc7c7c6b6c249d643d7a7dd
David Axelrod <daxaxelrod@gmail.com>
2017-02-14 13:01:04 -0500

    notes from finance 2-14-17. happy valentines day

--- Acct_212/2-13-17.py
+++ Acct_212/2-13-17.py
@@ -5,3 +5,6 @@ Act 212
 Class is going to be going over another large problem
 Wednesday is a review session
     Attendance is not required
+
+Annnnnnnnndddddd I didnt listen to a word he said
+    Just know how to write journal entries dealing with manufacturing overhead and when something is a period cost
---
+++ Fin324/2-14-17.py
@@ -0,0 +1,59 @@
+Fin 324
+2-14-17
+
+Going to go through the case
+    My group presents next thursday
+    In lecture 1 there is a framework on how to write the case
+
+Wrigley Jr. Company was the worlds largest manufactureer and seller of chewing gums
+    Case written in 2001
+    brands include orbit, juicy fruit, altoids, big league chew and double mint
+
+Chewing gum industry
+    Extremely competitive
+    Dominated by a few large players 50-60% of volume produced by a few players
+
+Revenue grew at 10% and earnings at 9%
+    Foreign sales is 58% of profit
+
+conservativly financed
+    No debt at the end of 2001
+    Founding family had a controlling stake
+        Dual class shares
+
+Some hedgefund named aurora borealis swoops in
+    In 2002 Some manager thing they can increase gains by adding corporate debt to wriglets balance sheet
+    under her plan
+        issue 3 billion in debt
+            either issue 3 billion in dividends
+            or authorize share buybacks
+
+Questions to answer
+    Write a problem statement
+    Should Wrigley pursue the $3 billion debt recapitalization proposal? In particular,
+        Should it issue the $3 billion corporate bond?
+        Does the recapitalization result in an “optimal capital structure”?
+        What would you recommend on the debt-equity mix?
+
+    Find optimal debt amount
+
+    Share repurchase has other ramifications
+        Including ownership and market responses
+
+    Consider impact of
+        Share value
+            Debt rating
+            Voting control
+        Cost of capital
+            eps
+
+This is not a new problems
+    There are comps out there
+
+Ways to consider the pv of distress
+    1.Can we ignore this with appealing arguments?
+
+    2.Guess From the debt ratings
+        Calculate cost of equity and then get the difference between that and a triple a rated bond
+        Singifies the expected risk of distress or bankruptcy
+        Be careful because if you find this difference to eb 4-5%, there may be other factors
