7efa0597cea7d039a35f20b67b96311119256ea9
David Axelrod <daxaxelrod@gmail.com>
2017-02-15 12:52:33 -0500

    notes through history 104 on 2-14-17

---
+++ Hist104/2-15-17.py
@@ -0,0 +1,46 @@
+Hist 104
+2-15-17
+
+IDs
+    Efficiency
+    muckrakers
+    settlement house
+    national consumers League
+    temperance campaign
+    19th amendment
+
+Progressivism
+    Swept through us as a reaction to industrialization
+    Both a reform movement and an ideology
+    See social problems as an environmental problem and can be solved by influencing environments
+    They study the citys in particular -- Trying to take the cities back
+        Zoning laws are a result
+    17th amendment passed
+        Senate voted in by popular vote
+
+Muckrakers
+    Root out curruption thorugh exoises
+
+National Consumer League
+    System of pressure on industries that employ child labor
+        also other shady working conditions
+    Collective boycotters
+    Also had a white label that informed consumers of the working condition standards
+        of a product's' factory
+
+Settlement house
+    Starts in england
+    Staffed by mostly middle class women
+        Provided social services, education and work with poor immigrants
+    Hull house was one of the larger ones
+    Had a nursery, medicine dispensary, art gallery kitchen
+    Tried to help immigrants assimilate
+    by 1910, <400 in the us
+
+Prohibition movement
+    Sought to end all alcohol production
+        Succeeds -- 18th ammendment passed
+
+19th Ammendment
+    Right for women to vote
+    Illegal to deny vote based on sex
