657b05b92fcffd0eb753f72fca5128426c421b43
David Axelrod <daxaxelrod@gmail.com>
2017-02-15 19:37:37 -0500

    notes through wednesday 2-15-17

---
+++ Fin_320/2-15-17.py
@@ -0,0 +1,40 @@
+Fin 320
+1-15-17
+
+Something is up with my eye
+
+Mid term next Wednesday (one week from today)
+    Home work 2 is due on monday
+
+Chapter 5
+    How do risk and term structure affect interest rates
+
+3 Theories
+    Expectations theory
+        Bonds are substituts for eachother even if the maturities are different
+        Assume you are going to buy 2 one year bonds vs 1 two year bond
+        i long term is the avergae of the short term bond interest rates
+    Market Segmentation Theory
+        Opposite of Expectations theory
+    Liquidiity Premium theory
+        There is a liquidiity premium added after you average the short term bonds
+        increases as the matruity increases
+
+Formula for expectation theory rehashed
+
+Rate 1 is year 1 bond interest rate
+Rate 1,2 ^e is the expected 1 year rate a year from now
+
+#not expecation theory??
+(1+rate1) (1+rate1,2^e) = (1 + rate2)^2
+
+#expectation theory
+Will always overestimate
+Rate 2 == (rate1 + rate1,2^e) / 2
+
+
+
+Chapter 9
+    Not on the test
+    What is the fed
+    FOMC vs governers and things of that nature
