4ca0d83029016b5c6a63a8fcb6039f83ac2c044a
David Axelrod <daxaxelrod@gmail.com>
2017-02-16 16:13:53 -0500

    notes from fin 324 about venture. pretty interesting actually

---
+++ Fin324/2-16-17.py
@@ -0,0 +1,60 @@
+Fin 324
+2-16-17
+
+Chapter 15
+Raising capital
+
+Venture capital
+    Fill in the early stage funding that banks wont provide
+
+    Private funding in new companies in exchange for equity
+
+    Things investors look for
+        Ideas : who would use the product
+        Source of referall
+            History of acomplishments
+        Quality of Entreprenuer
+        Exit Opportunities
+
+    redherring.com
+    pwcmoneytree.com
+
+    some paper found that if a vc is active in a startups hq,
+        They are more likely to have an exit and be more innovative
+
+How to choose a vc
+    Look for financial strength
+    Choose a compatible vc firm
+    Obtain checks and referenced
+    What contacts do they
+        Do they have contacts in your particular industry
+
+Selling securities to the public
+    Management must get permission from the board of directors
+    Firm must file a registration statement with the sec
+
+underwriters
+    Formulate method used to issue securities
+    Price the securities
+    sell the securities
+    Price stabilization lead by underwriters
+
+    Often within syndicate
+        Group of underwriters
+
+Firm commitment underwriting
+    Firm takes no risk, underwriter takes all the risk
+
+Best efforts underwriting
+    The underwriting company makes their best effort to sell securities
+
+Dutch option underwriting
+    Underwriteing company accepts a series of bids
+    Suppose google is going public and they want to raise $100 using 100 shares
+        What is the price you'd' pay and how many shares do you want
+        Someone says 10 shares for .50
+        another says 20 shares for .50
+        another says 60 shares for $1.50
+        the highest bidder gets their shares but the price is the highest price
+        that will result in all the shares being sold
+        
