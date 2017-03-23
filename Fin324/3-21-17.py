4e0fdb161457e0abbf34eda9120eba279932a4ce
David Axelrod <daxaxelrod@gmail.com>
2017-03-23 11:41:35 -0400

    notes up to 3-22-17

---
+++ Fin324/3-21-17.py
@@ -0,0 +1,23 @@
+Fin 324
+3-21-17
+
+Just got back from a meeting with KW
+He wants to be a partner
+    WOWOWOWOOOOOOHOOOOOOo
+
+Goes over the residual theory of dividends
+    Its just net income - whatever capital you need for capital investments
+    They you just pay out the remainder as dividends
+
+dividend Irrelevance theory
+    No frictions in this world
+    paying out dividends has not effect on value to shareholders
+
+    Image a company with one shareholder and 1 share
+        If that share generates a single dollar in income over 10 years
+
+        dividends paid are 100% of FCF
+            Value to shareholder is $1/year or $10
+        If the fcf is retained as earnings
+            Value to sharehold accumulates within the firm
+            
