0553701273254a22f5aa0d04c0fee4df5b3b15af
David Axelrod <daxaxelrod@gmail.com>
2017-03-29 12:02:05 -0400

    notes through the 27th of march

---
+++ Hist104/3-27-17.py
@@ -0,0 +1,25 @@
+3-27-17
+Hist 104
+
+The second red scare
+
+
+Early Roots
+    Constant distrust of radicals
+    Smith act
+        Makes it illegal to teach how to destroy united states government
+        in 1948 its used to target communist party members
+            Maxism and Linninism were seen as ideologies to overthrow the government
+
+    1947 - 1957
+        An ideology so always in people's minds'
+    Federal loyalty program
+        Truman was suspicious, all federal employees were heavily screened
+        People were fired based on 'disloyalty' with no appeal process
+    1948
+        Ussr tests first atomic bomb
+    CHINA falls to communism around this time too
+
+    Hatred of gays
+        Lavender scare
+        People thought gay people were communists
