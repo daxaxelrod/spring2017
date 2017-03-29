21164139e7113ece128610f8ec6e55bb30d61436
David Axelrod <daxaxelrod@gmail.com>
2017-03-29 17:52:14 -0400

    History for 3-29-17

---
+++ Hist104/3-29-17.py
@@ -0,0 +1,71 @@
+Hist 104
+3-29-17
+
+
+Cival Rights movement
+
+NAACP challenged things in court
+    Tons of lawyers
+Congress of racial equality (CORE)
+
+Why did the cival rights movement rise in the 1950s
+    A lot a decolonization efforts
+    Brown v board of ed overturned separate but equal
+        Makes the opposition angrier and more violent
+    Citizens councils
+        White racist fucks who stopped blacks from voting and running businesses
+    Lynching of emmett Till
+        14 year old boy murdered for harassing a white woman in mississippi
+        Murderers are freed by mistrial, openly admitted to murder
+        Till mother put his body in an open coffin
+            Reinforces southern brutality
+
+Montgomery bus boycott (55-56)
+    Mlk gets his start
+    Rosa parks was not tired but she tired of being pushed around
+
+Little Rock 9
+    Black students try to enter a white school
+    Racial and violent backlash
+    Eisenhower sends in 1000 federal soldiers under cold war pressure
+
+CRM strats
+    Nonviolence
+        Trained activists to remain silent and clam in the face of aggression
+    Sit ins
+        Technique where young black students would sit somewhere and refuse to move
+Birmingham campaign 1963
+    Kennedy reluctant to take action
+    MLK and the SCLC want to launch something huge to force federal action
+    Tons of people arrested that they were running out of protesters
+    MLK organized young children to walk with them
+        The police flips the fuck out (fucking retard) and sends dogs, water hoses and electric cattle prods
+
+March on washington (1963)
+    March for jobs and freedom, in august
+    Numbers show support (300k people)
+    Still violence in the south, september church bombing that
+        killed 4 girls who were attending sunday school
+
+1964 Civil rights act passed
+    JFK is assasinated in 1963
+    Lyndon b johnson
+    The act banned racial discrimination and segregation
+    Gave government powers to fight school segregation
+    Created EEOC (Equal employement opportunity commision)
+    Did not guarantee right to vote
+
+Freedom Summer project
+    Major campaign to register voters
+    Bloody sunday
+        Local decon was shot by a state trooper
+        600 march and are hit with tear gas whips and barbed wire
+        All televised
+        Staged a march from selma to Montgomery
+
+Voting rights act of 1965
+    Johnson pushed through congress
+    Outlaws literacy tests
+    Justice dept monitored elections
+    Black and white registration numbers soon is equal
+    Both acts end de jure(in theory) segregation but race relations wont be solved by federal bills
