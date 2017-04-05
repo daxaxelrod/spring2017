75d156ad39f9d94146a5bfc97db9537c0f3e735a
David Axelrod <daxaxelrod@gmail.com>
2017-04-05 18:56:08 -0400

    notes through the day before tibah leaves for efest. WOOOOooooOOO

---
+++ Hist104/4-5-17.py
@@ -0,0 +1,16 @@
+4-5-17
+Hist 104
+
+Legislation of the great society, War on poverty
+
+Economic opportunity (OEO) 1964
+    Creates programs like head start (pre school for impoversihed families)
+    Community action program(CAP) integrated porr into local communities
+    Jobs corps (jobs for young americans)
+Elementary and secondary education act 1965
+    "Freedom against ignorance" -LBJ
+    sends federal dollars to schools with 40% or more poor chilren, title 1 designation
+    1.3 billion spent
+Higher ed action
+    Expands federal funding to college and universities for programs scholarships and new buildings
+    creates pell grants for low income students
